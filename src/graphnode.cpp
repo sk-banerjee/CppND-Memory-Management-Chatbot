6a7fb74b1f9cae016436eabdcb0f72d20ed1ee31
Sourabh Banerjee <banerjee.sourabh@gmial.com>
2021-04-25 17:36:08 +0530

    graphnode: fix crash on window close
    graphnode destructor should not delete _chatBot

--- src/graphnode.cpp
+++ src/graphnode.cpp
@@ -11,7 +11,8 @@ GraphNode::~GraphNode()
     //// STUDENT CODE
     ////
 
-    delete _chatBot; 
+    // Do NOT delete _chatBot here as it is not constructed here.
+    // delete _chatBot;
 
     ////
     //// EOF STUDENT CODE
